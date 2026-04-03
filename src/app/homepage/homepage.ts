d59282373b4e9de4a2c55f3488c504687aef6305
Diana Durbaca <dianadurbaca2003@gmail.com>
2026-04-03 16:47:54 +0300

    feat: enhance homepage with mobile menu functionality and responsive design adjustments

--- src/app/homepage/homepage.ts
+++ src/app/homepage/homepage.ts
@@ -1,9 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './homepage.html',
   styleUrl: './homepage.css',
 })
-export class Homepage {}
+export class Homepage {
+  mobileMenuOpen = false;
+
+  toggleMobileMenu() {
+    this.mobileMenuOpen = !this.mobileMenuOpen;
+  }
+
+  closeMobileMenu() {
+    this.mobileMenuOpen = false;
+  }
+}
