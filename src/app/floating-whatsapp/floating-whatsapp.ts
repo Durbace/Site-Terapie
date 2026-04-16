40b50ef5cab2e57eab035b38ad4a2891043bb3aa
Diana Durbaca <dianadurbaca2003@gmail.com>
2026-04-16 14:01:05 +0300

    feat: add Floating WhatsApp component with styles and integrate into app

---
+++ src/app/floating-whatsapp/floating-whatsapp.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-floating-whatsapp',
+  standalone: true,
+  templateUrl: './floating-whatsapp.html',
+  styleUrl: './floating-whatsapp.css'
+})
+export class FloatingWhatsappComponent {}
