bca608053b8ff714158a4347caad698ff1034398
Diana Durbaca <dianadurbaca2003@gmail.com>
2026-04-17 16:09:33 +0300

    feat: enhance osteopatie page with RevealOnScroll directive and new hero images

--- src/app/osteopatie/osteopatie.ts
+++ src/app/osteopatie/osteopatie.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 
 import { NavbarComponent } from '../navbar/navbar';
 import { FooterComponent } from '../footer/footer';
+import { RevealOnScrollDirective } from '../../reveal-on-scroll.directive';
 
 @Component({
   selector: 'app-osteopatie',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, FooterComponent],
+  imports: [CommonModule, NavbarComponent, FooterComponent, RevealOnScrollDirective],
   templateUrl: './osteopatie.html',
   styleUrl: './osteopatie.css',
 })
